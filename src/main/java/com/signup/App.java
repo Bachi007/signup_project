26fa601df56241cc22677119b6a333c33b1284ed
Bachi007 <chitturi.bhaskar007@gmail.com>
2022-08-18 15:20:37 +0530

    signup project

---
+++ src/main/java/com/signup/App.java
@@ -0,0 +1,48 @@
+package com.signup;
+
+/**
+ * Hello world!
+ *
+ */
+import java.util.Scanner;
+import static java.lang.System.*;
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Scanner sc=new Scanner(System.in);
+    	signupvalidations sv= new signupvalidations();
+    	out.println("Welcome to Signup Page");
+        out.println("Enter Username ");
+        String username=sc.next();
+        try {
+        	if(sv.checkUsername(username)) {
+        		out.println("Enter Password");
+        		String password=sc.next();
+        		try {
+        			if(sv.checkPassword(password))
+        			{
+        				out.println("Phone Number");
+        				String phone=sc.next();
+        				try {
+        					if(sv.checkPhone(phone))
+        					{
+        						out.println("Signup successful");
+        						out.println("username :"+username+"password"+password+"phone number"+phone);
+        					}
+        				}
+        				catch(Exception e) {
+        					out.println(e.getMessage());
+        				}
+        			}
+        		}
+        		catch(Exception e) {
+        			out.println(e.getMessage());
+        		}
+        	}
+        }
+        catch(Exception e){
+        	out.println(e.getMessage());
+        }
+    }
+}
